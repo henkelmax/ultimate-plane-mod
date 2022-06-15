b54dc56a95b6d12f03b424db7bf5a691e308088a
Max Henkel <max@maxhenkel.de>
2022-06-15 10:35:38 +0200

    Re add Jade integration

--- src/main/java/de/maxhenkel/plane/integration/waila/HUDHandlerPlanes.java
+++ src/main/java/de/maxhenkel/plane/integration/waila/HUDHandlerPlanes.java
@@ -1,35 +1,36 @@
 package de.maxhenkel.plane.integration.waila;
 
 import de.maxhenkel.corelib.math.MathUtils;
+import de.maxhenkel.plane.Main;
 import de.maxhenkel.plane.entity.EntityPlane;
-import mcp.mobius.waila.Waila;
-import mcp.mobius.waila.api.EntityAccessor;
-import mcp.mobius.waila.api.IEntityComponentProvider;
-import mcp.mobius.waila.api.ITooltip;
-import mcp.mobius.waila.api.TooltipPosition;
-import mcp.mobius.waila.api.config.IPluginConfig;
 import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import snownee.jade.api.EntityAccessor;
+import snownee.jade.api.IEntityComponentProvider;
+import snownee.jade.api.ITooltip;
+import snownee.jade.api.config.IPluginConfig;
 
 public class HUDHandlerPlanes implements IEntityComponentProvider {
 
-    public static final ResourceLocation OBJECT_NAME_TAG = new ResourceLocation("waila", "object_name");
+    public static final ResourceLocation OBJECT_NAME_TAG = new ResourceLocation("jade", "object_name");
 
     public static final HUDHandlerPlanes INSTANCE = new HUDHandlerPlanes();
 
+    public static final ResourceLocation UID = new ResourceLocation(Main.MODID, "plane");
+
     @Override
     public void appendTooltip(ITooltip iTooltip, EntityAccessor entityAccessor, IPluginConfig iPluginConfig) {
-        if (entityAccessor.getTooltipPosition().equals(TooltipPosition.HEAD)) {
+        if (entityAccessor.getEntity() instanceof EntityPlane plane) {
             iTooltip.remove(OBJECT_NAME_TAG);
-            iTooltip.add(new TextComponent(String.format(Waila.CONFIG.get().getFormatting().getBlockName(), entityAccessor.getEntity().getDisplayName().getString())).withStyle(ChatFormatting.WHITE));
-        } else if (entityAccessor.getTooltipPosition().equals(TooltipPosition.BODY)) {
-            if (entityAccessor.getEntity() instanceof EntityPlane plane) {
-                iTooltip.add(new TranslatableComponent("tooltip.plane.fuel", new TextComponent(String.valueOf(plane.getFuel())).withStyle(ChatFormatting.DARK_GRAY)).withStyle(ChatFormatting.GRAY));
-                iTooltip.add(new TranslatableComponent("tooltip.plane.damage", new TextComponent(String.valueOf(MathUtils.round(plane.getPlaneDamage(), 2))).withStyle(ChatFormatting.DARK_GRAY)).withStyle(ChatFormatting.GRAY));
-            }
+            iTooltip.add(entityAccessor.getEntity().getDisplayName().copy().withStyle(ChatFormatting.WHITE));
+            iTooltip.add(Component.translatable("tooltip.plane.fuel", Component.literal(String.valueOf(plane.getFuel())).withStyle(ChatFormatting.DARK_GRAY)).withStyle(ChatFormatting.GRAY));
+            iTooltip.add(Component.translatable("tooltip.plane.damage", Component.literal(String.valueOf(MathUtils.round(plane.getPlaneDamage(), 2))).withStyle(ChatFormatting.DARK_GRAY)).withStyle(ChatFormatting.GRAY));
         }
     }
 
+    @Override
+    public ResourceLocation getUid() {
+        return UID;
+    }
 }
--- src/main/java/de/maxhenkel/plane/integration/waila/PluginPlane.java
+++ src/main/java/de/maxhenkel/plane/integration/waila/PluginPlane.java
@@ -1,34 +1,16 @@
 package de.maxhenkel.plane.integration.waila;
 
 import de.maxhenkel.plane.entity.EntityPlaneSoundBase;
-import mcp.mobius.waila.api.IRegistrar;
-import mcp.mobius.waila.api.IWailaPlugin;
-import mcp.mobius.waila.api.TooltipPosition;
-import mcp.mobius.waila.api.WailaPlugin;
-import mcp.mobius.waila.api.event.WailaRenderEvent;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.phys.EntityHitResult;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import snownee.jade.api.IWailaClientRegistration;
+import snownee.jade.api.IWailaPlugin;
+import snownee.jade.api.WailaPlugin;
 
 @WailaPlugin
 public class PluginPlane implements IWailaPlugin {
 
     @Override
-    public void register(IRegistrar registrar) {
-        registrar.registerComponentProvider(HUDHandlerPlanes.INSTANCE, TooltipPosition.HEAD, EntityPlaneSoundBase.class);
-        registrar.registerComponentProvider(HUDHandlerPlanes.INSTANCE, TooltipPosition.BODY, EntityPlaneSoundBase.class);
-        registrar.registerComponentProvider(HUDHandlerPlanes.INSTANCE, TooltipPosition.TAIL, EntityPlaneSoundBase.class);
-    }
-
-    @SubscribeEvent
-    public void onWailaRender(WailaRenderEvent.Pre event) {
-        if (event.getAccessor().getHitResult() instanceof EntityHitResult result) {
-            if (result.getEntity() instanceof EntityPlaneSoundBase plane) {
-                if (plane.getPassengers().contains(Minecraft.getInstance().player)) {
-                    event.setCanceled(true);
-                }
-            }
-        }
+    public void registerClient(IWailaClientRegistration registration) {
+        registration.registerEntityComponent(HUDHandlerPlanes.INSTANCE, EntityPlaneSoundBase.class);
     }
 
 }
